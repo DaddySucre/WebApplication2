eaaad0a592e1ea38339773847fbc4a97dc890b15
Mickaël Lebailly <mickael.lebailly22@gmail.com>
2018-05-17 15:33:31 +0200

    fdsfdsfds

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -16,6 +16,8 @@ namespace WebApplication2.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page.";
 
             return View();
         }
